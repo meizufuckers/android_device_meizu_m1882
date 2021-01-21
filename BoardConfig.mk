e31f10f83c0045a69d48df9cac2e86001c0923af
Andreas Schneider <asn@cryptomilk.org>
2021-01-21 08:55:39 +0100

    sdm845: sepolicy: Label lineage fastcharge HAL
    Change-Id: I428e54273156cff2d25897f62ecff2ba4dc53629

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -165,6 +165,7 @@ VENDOR_SECURITY_PATCH := 2020-10-01
 # SELinux
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
+BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += $(DEVICE_PATH)
