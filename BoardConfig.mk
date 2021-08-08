95d152715b081260fdbac70eaec0b1f7013d4bb8
TRANSAERO21 <martynenkodima21@gmail.com>
2021-08-09 02:09:54 +0700

    sdm845: Exclude qcom sepolicy

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -181,7 +181,6 @@ ENABLE_VENDOR_RIL_SERVICE := true
 VENDOR_SECURITY_PATCH := 2020-10-01
 
 # SELinux
-include device/qcom/sepolicy_vndr/SEPolicy.mk
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/public
