b40d5892b817f590af9de3ed5f08825214a95b1a
TRANSAERO21 <martynenkodima21@gmail.com>
2021-12-24 23:12:26 +0300

    sdm845: clean up properties, round 2

--- m1882.mk
+++ m1882.mk
@@ -4,3 +4,7 @@ $(call inherit-product, device/meizu/sdm845/sdm845.mk)
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay-m1882
+
+# Meizu property
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.product.flyme.model=M1882
